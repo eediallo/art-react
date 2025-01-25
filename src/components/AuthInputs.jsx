f9fa5be87e283cd59f8bb2e4e6bc697989cca056
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-25 15:22:22 +0000

    refactor by creating Label styled component

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -8,6 +8,16 @@ const ControlsDiv = styled.div`
   margin-bottom: 1.5rem;
 `;
 
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: #6b7280;
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -32,9 +42,9 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlsDiv className="controls">
         <p>
-          <label className={`label ${emailNotValid ? "invalid" : ""}`}>
+          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>
             Email
-          </label>
+          </Label>
           <input
             type="email"
             className={emailNotValid ? "invalid" : undefined}
@@ -42,9 +52,9 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <label className={`label ${passwordNotValid ? "invalid" : ""}`}>
+          <Label className={`label ${passwordNotValid ? "invalid" : ""}`}>
             Password
-          </label>
+          </Label>
           <input
             type="password"
             className={passwordNotValid ? "invalid" : undefined}
